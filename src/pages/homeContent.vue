e95894ec2c1da1dcccce818ce3d77a0b35bfd1b9
Vlad Mnatsakanyan <mnatsakanyan@yual.io>
2024-12-11 16:29:16 +0200

    UICards

--- src/pages/homeContent.vue
+++ src/pages/homeContent.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>
-    hello home
+  <div class="md-body">
+    <h1>hello home</h1>
+    <p>Edit me</p>
+    <div v-for="item in items" :key="item.id" class="card_wrapper">
+      <UserCard :name="`${item.lvl} lvl`" :title="item.title" :description="item.description" />
+    </div>
   </div>
 </template>
+
+<script>
+import UserCard from '@/components/UI/UserCard.vue';
+export default {
+  components: {
+    UserCard
+  },
+  data() {
+    return {
+      items: [
+      {
+        id: 1,
+        title: 'Archer',
+        description: 'archer archer archer',
+        lvl: 4
+      },
+      {
+        id: 2,
+        title: 'Wizard',
+        description: 'wizard wizard wizard',
+        lvl: 5
+      }
+      ]
+    }
+  }
+}
+</script>
