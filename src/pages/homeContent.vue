f061a3eed4388a00c6d636ea3c3d02e916231946
Vlad Mnatsakanyan <mnatsakanyan@yual.io>
2024-12-11 20:56:45 +0200

    img

--- src/pages/homeContent.vue
+++ src/pages/homeContent.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="md-body">
-    <h1>hello home</h1>
-    <p>Edit me</p>
-    <div v-for="item in items" :key="item.id" class="card_wrapper">
-      <UserCard :name="`${item.lvl} lvl`" :title="item.title" :description="item.description">
+    <div v-for="item in items" :key="item.id" class="card__wrapper">
+      <UserCard 
+        :name="`${item.lvl} lvl`" 
+        :title="item.title" 
+        :imgUrl="item.img">
         {{ item.description }}
       </UserCard>
     </div>
@@ -21,14 +22,16 @@ export default {
       items: [
       {
         id: 1,
-        title: 'Archer',
+        title: 'HTML',
         description: 'archer archer archer',
+        img: require('@/assets/img/HTML5.webp'),
         lvl: 4
       },
       {
         id: 2,
-        title: 'Wizard',
+        title: 'VUE',
         description: 'wizard wizard wizard',
+        img: require('@/assets/img/Vue.js_Logo_2.svg.png'),
         lvl: 5
       }
       ]
